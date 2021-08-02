f88b3b31176566d8f3642834e87009f50673d512
bengdawson <bengdawson@gmail.com>
2021-08-02 17:34:45 +0100

    Resolved issue with lambda/apigateway, created /users endpoint.

--- api.js
+++
@@ -1,4 +0,0 @@
-api.get("/greet", function (request) {
-  var superb = require("superb");
-  return request.queryString.name + " is " + superb.random();
-});
--- app.js
+++ app.js
@@ -1,14 +1,30 @@
-/* var ApiBuilder = require("claudia-api-builder"),
-  api = new ApiBuilder();
+const ApiBuilder = require("claudia-api-builder");
+const api = new ApiBuilder();
+const mysql = require("mysql2/promise");
+const express = require("express");
+
+const pool = require("./middleware/database.js").pool;
+
+api.get(
+  "/users",
+  async function () {
+    const rows = await pool.query("SELECT * FROM users;");
+    const result = Object.values(JSON.parse(JSON.stringify(rows[0])));
+    return { users: result };
+  },
+  { success: { contentType: "application/json" }, error: { code: 403 } }
+);
+
+// async function userTest() {
+//   const rows = await pool.query("SELECT * FROM users;");
+//   const result = Object.values(JSON.parse(JSON.stringify(rows[0])));
+//   return result;
+// }
+
+// userTest();
+
+api.get("/", function () {
+  return "Hello world!";
+});
 
 module.exports = api;
-
-api.get("/hello", function () {
-  return "hello world";
-});
-
-api.get("/greet", function (request) {
-  var superb = require("superb");
-  return request.queryString.name + " is " + superb.random();
-});
- */
