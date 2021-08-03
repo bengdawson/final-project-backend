d60977a356a1fff15a05638369b4857cc7611a09
Sojeong Park <macpark09@gmail.com>
2021-08-03 11:06:13 +0100

    eslint and husky

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
 const ApiBuilder = require("claudia-api-builder");
 const api = new ApiBuilder();
-const mysql = require("mysql2/promise");
-const express = require("express");
+//const mysql = require("mysql2/promise");
+//const express = require("express");
 const pool = require("./middleware/pool");
 
 console.log(pool);
 
 api.get(
-  "/users",
-  async function () {
-    const rows = await pool.query("SELECT * FROM users;");
-    const result = Object.values(JSON.parse(JSON.stringify(rows[0])));
-    return { users: result };
-  },
-  { success: { contentType: "application/json" }, error: { code: 403 } }
+	"/users",
+	async function () {
+		const rows = await pool.query("SELECT * FROM users;");
+		const result = Object.values(JSON.parse(JSON.stringify(rows[0])));
+		return { users: result };
+	},
+	{ success: { contentType: "application/json" }, error: { code: 403 } }
 );
 
 api.get(
-  "/orgs",
-  async function () {
-    const rows = await pool.query("SELECT * FROM orgs;");
-    const result = Object.values(JSON.parse(JSON.stringify(rows[0])));
-    return { orgs: result };
-  },
-  { success: { contentType: "application/json" }, error: { code: 403 } }
+	"/orgs",
+	async function () {
+		const rows = await pool.query("SELECT * FROM orgs;");
+		const result = Object.values(JSON.parse(JSON.stringify(rows[0])));
+		return { orgs: result };
+	},
+	{ success: { contentType: "application/json" }, error: { code: 403 } }
 );
 
 api.get("/", function () {
-  return "Hello world!";
+	return "Hello world!";
 });
 
 module.exports = api;
--- middleware/pool.js
+++ middleware/pool.js
@@ -5,22 +5,22 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const pool = mysql.createPool({
-  connectionLimit: 10,
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+	connectionLimit: 10,
+	host: process.env.DB_HOST,
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DATABASE,
 });
 
 pool.on("connection", function (connection) {
-  console.log("DB Connection established");
+	console.log("DB Connection established");
 
-  connection.on("error", function (err) {
-    console.error(new Date(), "MySQL error", err.code);
-  });
-  connection.on("close", function (err) {
-    console.error(new Date(), "MySQL close", err);
-  });
+	connection.on("error", function (err) {
+		console.error(new Date(), "MySQL error", err.code);
+	});
+	connection.on("close", function (err) {
+		console.error(new Date(), "MySQL close", err);
+	});
 });
 
 module.exports = pool;
