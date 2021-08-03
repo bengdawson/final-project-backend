60f95a0fe4f8f186c078bc872262665f873f731d
Sojeong Park <macpark09@gmail.com>
2021-08-03 10:25:56 +0100

    api access

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-var ApiBuilder = require("claudia-api-builder"),
-  api = new ApiBuilder();
+const ApiBuilder = require("claudia-api-builder");
+const api = new ApiBuilder();
+const mysql = require("mysql2/promise");
+const express = require("express");
+const pool = require("./middleware/pool");
 
-module.exports = api;
+console.log(pool);
 
- api.get("/hello", function () {
-  return "hello world";
+api.get(
+  "/users",
+  async function () {
+    const rows = await pool.query("SELECT * FROM users;");
+    const result = Object.values(JSON.parse(JSON.stringify(rows[0])));
+    return { users: result };
+  },
+  { success: { contentType: "application/json" }, error: { code: 403 } }
+);
+
+api.get(
+  "/orgs",
+  async function () {
+    const rows = await pool.query("SELECT * FROM orgs;");
+    const result = Object.values(JSON.parse(JSON.stringify(rows[0])));
+    return { orgs: result };
+  },
+  { success: { contentType: "application/json" }, error: { code: 403 } }
+);
+
+api.get("/", function () {
+  return "Hello world!";
 });
 
-api.get("/greet", function (request) {
-  var superb = require("superb");
-  return request.queryString.name + " is " + superb.random();
-}); 
- 
-
+module.exports = api;
